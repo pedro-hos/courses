0f1074e07b0cf5bb6d468cd77d4aa03de7c3ddd4
pedro-hos <pedro-hos@outlook.com>
2016-12-04 14:18:35 -0200

    cap 02 es6

--- alura/JavaScript/ES6 orientação a objetos e padrões de projetos/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ alura/JavaScript/ES6 orientação a objetos e padrões de projetos/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -1,15 +1,29 @@
 class NegociacaoController {
 
   constructor() {
-
+    let self = this;
     let $ = document.querySelector.bind(document);
     this._inputData = $('#data');
     this._inputQuantidade = $('#quantidade');
     this._inputValor = $('#valor');
 
     this._negociacoesView = new NegociacoesView($('#negociacoesView'));
-    this._listaNegociacoes = new ListaNegociacoes(model => this._negociacoesView.update(model));
-  
+    this._listaNegociacoes = new Proxy(new ListaNegociacoes(), {
+      
+      get(target, prop, receiver) {
+        if(['esvazia', 'adiciona'].includes(prop) && typeof(target[prop]) === typeof(Function)) {
+
+          return function() {
+            Reflect.apply(target[prop], target, arguments);
+            self._negociacoesView.update(target);
+          }
+
+        }
+
+        return Reflect.get(target, prop, receiver);
+      }
+    });
+
     this._mensagem = new Mensagem();
     this._mensagemView = new MensagemView($('#mensagemView'));
     this._mensagemView.update(this._mensagem);
